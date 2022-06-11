6780da436ec7bf1bb366bd84fdbd1c7bebd90b01
Emily <tweetychrip@gmail.com>
2022-06-10 17:04:12 -0700

    remove rotation

--- grid_movement/pawns/Walker.gd
+++ grid_movement/pawns/Walker.gd
@@ -2,15 +2,11 @@ extends Pawn
 
 onready var parent = get_parent()
 
-func _ready():
-	update_look_direction(Vector2.RIGHT)
-
 
 func _process(_delta):
 	var input_direction = get_input_direction()
 	if not input_direction:
 		return
-	update_look_direction(input_direction)
 
 	var target_position = parent.request_move(self, input_direction)
 	if target_position:
@@ -27,15 +23,12 @@ func get_input_direction():
 	)
 
 
-func update_look_direction(direction):
-	$Pivot/Sprite.rotation = direction.angle()
-
 
 func move_to(target_position):
 	set_process(false)
 	$AnimationPlayer.play("walk")
 	var move_direction = (position - target_position).normalized()
-	$Tween.interpolate_property($Pivot, "position", move_direction * 32, Vector2(), $AnimationPlayer.current_animation_length, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.interpolate_property($Pivot, "position", move_direction * 16, Vector2(), $AnimationPlayer.current_animation_length, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Pivot/Sprite.position = position - target_position
 	position = target_position
 
